b68ab687bb128e2cee69b6338b7562358c593c65
Shengzhe Chen <schen@ancestry.com>
2015-02-03 14:21:19 -0800

    update

---
+++ AbstractFactory/AbstractFactory/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AbstractFactory
+//
+//  Created by Shengzhe Chen on 2/3/15.
+//  Copyright (c) 2015 Shengzhe Chen. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
