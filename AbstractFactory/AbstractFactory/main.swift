ad3e1dd92d792255bbe3bb8f4a1de41689e51344
Shengzhe Chen <schen@ancestry.com>
2015-02-03 16:14:41 -0800

    done with abstractfactory

--- AbstractFactory/AbstractFactory/main.swift
+++ AbstractFactory/AbstractFactory/main.swift
@@ -8,5 +8,65 @@
 
 import Foundation
 
-println("Hello, World!")
+public protocol AbstractProductA {
+    
+}
 
+public protocol AbstractProductB {
+
+}
+
+public protocol AbstractFactory {
+    func CreateProductA() -> AbstractProductA
+    func CreateProductB() -> AbstractProductB
+}
+
+class ConcreteProductA1 : AbstractProductA {
+    
+}
+
+class ConcreteProductB1 : AbstractProductB {
+    
+}
+
+class ConcreteProductA2 : AbstractProductA {
+    
+}
+
+class ConcreteProductB2 : AbstractProductB {
+    
+}
+
+class ConcreteFactory1 : AbstractFactory {
+    func CreateProductA() -> AbstractProductA {
+        return ConcreteProductA1()
+    }
+    func CreateProductB() -> AbstractProductB {
+        return ConcreteProductB1()
+    }
+}
+
+class ConcreteFactory2 : AbstractFactory {
+    func CreateProductA() -> AbstractProductA {
+        return ConcreteProductA2()
+    }
+    func CreateProductB() -> AbstractProductB {
+        return ConcreteProductB2()
+    }
+}
+
+class Client {
+    
+    var fs : [AbstractFactory] = [ConcreteFactory1(), ConcreteFactory2()]
+    func run() {
+        
+        for f in fs {
+            println(f)
+            println("----\(f.CreateProductA())")
+            println("----\(f.CreateProductB())")
+        }
+    }
+}
+
+var c = Client()
+c.run()
